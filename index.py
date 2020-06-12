3b7d4292d6df0fa9d015f12ab100600253322f7c
wgmb745 <66256132+wgmb745@users.noreply.github.com>
2020-06-12 15:06:19 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,16 +1,12 @@
 import json
 import datetime
 
-def calcular(n):
-    n += 10
-    return n
-
 def handler(event, context):
     data = {
         'output': 'Hello World',
         'timestamp': datetime.datetime.utcnow().isoformat()
+    }
     
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
-
