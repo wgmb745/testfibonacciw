b293a9ec3761e2e7ad4710c95ca0a002a29ddf48
wgmb745 <66256132+wgmb745@users.noreply.github.com>
2020-06-13 17:56:22 -0500

    Update index.py

--- index.py
+++ index.py
@@ -2,25 +2,22 @@ import json
 import datetime
 
 def handler(event, context):
-    
     n = int(1)
     if event is not None:
         n = int(event["queryStringParameters"]["numfib"])
-    
     lista = []
     cadena = ''
-    
-    # Ciclo para cadena de fibonacci
     a, b = 0,1
     while a < n:
         #print(a, end=' ')
         cadena = cadena + str(a) + ' '
         a, b = b, a+b
-    fibo = lista.append(cadena)
+    lista.append(cadena)
     
     data = {
-        #'fibonacci': fibo,
-        'numfib': cadena,
+        'result': result
+        'numfib': lista,
+        'output':'Test'
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     
